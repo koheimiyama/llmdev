51efdc8414e9ee8e81fb5d1f20d5756e0d3a797a
koheimiyama <kmiyama@lycorp.co.jp>
2025-05-12 18:47:12 +0900

    テストコードの課題完了

--- 06_test/test_authenticator.py
+++ 06_test/test_authenticator.py
@@ -5,8 +5,7 @@ from authenticator import Authenticator
 def test_register():
     auth = Authenticator()
     auth.register(username="user1", password="pass1")
-    result = auth.login("user1", "pass1")
-    assert "ログイン成功" == result
+    assert "user1" in auth.users
     with pytest.raises(ValueError, match="エラー: ユーザーは既に存在します。"):
         auth.register("user1", "pass1")
 
