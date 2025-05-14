2fdd76c089d5dac889dadc1e11f08d2792ce58c9
koheimiyama <kmiyama@lycorp.co.jp>
2025-05-14 21:58:51 +0900

    テスト分割

--- 06_test/test_authenticator.py
+++ 06_test/test_authenticator.py
@@ -2,20 +2,29 @@ import pytest
 from authenticator import Authenticator
 
 
-def test_register():
+@pytest.fixture
+def authenticator():
     auth = Authenticator()
-    auth.register(username="user1", password="pass1")
-    assert "user1" in auth.users
+    auth.register("user1", "pass1")
+    return auth
+
+
+def test_register_normal(authenticator):
+    assert "user1" in authenticator.users
+
+
+def test_register_error(authenticator):
     with pytest.raises(ValueError, match="エラー: ユーザーは既に存在します。"):
-        auth.register("user1", "pass1")
+        authenticator.register("user1", "pass1")
 
 
-def test_login():
-    auth = Authenticator()
-    auth.register(username="user1", password="pass1")
-    result = auth.login("user1", "pass1")
+def test_login_normal(authenticator):
+    result = authenticator.login("user1", "pass1")
     assert "ログイン成功" == result
+
+
+def test_login_error(authenticator):
     with pytest.raises(
         ValueError, match="エラー: ユーザー名またはパスワードが正しくありません。"
     ):
-        auth.login("user1", "passng")
+        authenticator.login("user1", "passng")
